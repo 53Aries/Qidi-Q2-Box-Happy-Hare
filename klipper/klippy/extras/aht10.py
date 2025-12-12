80eed9593d5eb554be2361f6a42447a968c447aa
53Aries <110708581+53Aries@users.noreply.github.com>
2025-12-11 22:20:10 -0600

    Add fast-sampling AHT sensor for heater control with humidity support

--- klipper/klippy/extras/aht10.py
+++ klipper/klippy/extras/aht10.py
@@ -1,6 +1,8 @@
-# AHT10/AHT20/AHT21 I2c-based humiditure sensor support
+# AHT10/AHT20/AHT21 I2c-based humidity/temperature sensor support
+# Modified for faster sampling suitable for heater control
 #
 # Copyright (C) 2023 Scott Mudge <mail@scottmudge.com>
+# Modified for faster heater control
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 import logging
@@ -9,19 +11,19 @@ from . import bus
 ######################################################################
 # Compatible Sensors:
 #       AHT10      -    Tested w/ BTT GTR 1.0 MCU on i2c3
-#       AHT20      -    Untested but should work
+#       AHT20      -    Tested
 #       AHT21      -    Tested w/ BTT GTR 1.0 MCU on i2c3
 ######################################################################
 
-AHT10_I2C_ADDR= 0x38
+AHT10_I2C_ADDR = 0x38
 
 AHT10_COMMANDS = {
-    'INIT'              :[0xE1, 0x08, 0x00],
-    'MEASURE'           :[0xAC, 0x33, 0x00],
-    'RESET'             :[0xBA, 0x08, 0x00]
+    'INIT': [0xE1, 0x08, 0x00],
+    'MEASURE': [0xAC, 0x33, 0x00],
+    'RESET': [0xBA, 0x08, 0x00]
 }
 
-AHT10_MAX_BUSY_CYCLES= 5
+AHT10_MAX_BUSY_CYCLES = 5
 
 class AHT10:
     def __init__(self, config):
@@ -30,14 +32,19 @@ class AHT10:
         self.reactor = self.printer.get_reactor()
         self.i2c = bus.MCU_I2C_from_config(
             config, default_addr=AHT10_I2C_ADDR, default_speed=100000)
-        self.report_time = config.getint('aht10_report_time',30,minval=5)
+        # Modified: Allow faster reporting for heater control (min 1 second)
+        self.report_time = config.getfloat('aht10_report_time', 5., minval=1.)
         self.temp = self.min_temp = self.max_temp = self.humidity = 0.
         self.sample_timer = self.reactor.register_timer(self._sample_aht10)
         self.printer.add_object("aht10 " + self.name, self)
+        if config.get('sensor_type', None) == 'AHT2X':
+            # Also register as aht2x for compatibility
+            self.printer.add_object("aht2x " + self.name, self)
         self.printer.register_event_handler("klippy:connect",
                                             self.handle_connect)
-        self.is_calibrated  = False
+        self.is_calibrated = False
         self.init_sent = False
+        self._callback = None
 
     def handle_connect(self):
         self._init_aht10()
@@ -58,7 +65,6 @@ class AHT10:
             return False
 
         data = None
-
         is_busy = True
         cycles = 0
 
@@ -68,7 +74,7 @@ class AHT10:
                 # and issue warning.
                 if is_busy and cycles > AHT10_MAX_BUSY_CYCLES:
                     logging.warning("aht10: device reported busy after " +
-                        "%d cycles, resetting device"% AHT10_MAX_BUSY_CYCLES)
+                        "%d cycles, resetting device" % AHT10_MAX_BUSY_CYCLES)
                     self._reset_device()
                     data = None
                     break
@@ -76,8 +82,8 @@ class AHT10:
                 cycles += 1
                 # Write command for updating temperature+status bit
                 self.i2c.i2c_write(AHT10_COMMANDS['MEASURE'])
-                # Wait 110ms after first read, 75ms minimum
-                self.reactor.pause(self.reactor.monotonic() + .110)
+                # Wait 85ms after first read (minimum 75ms, typical 85ms)
+                self.reactor.pause(self.reactor.monotonic() + .085)
 
                 # Read data
                 read = self.i2c.i2c_read([], 6)
@@ -88,7 +94,7 @@ class AHT10:
                 data = bytearray(read['response'])
                 if len(data) < 6:
                     logging.warning("aht10: received bytes less than" +
-                                    " expected 6 [%d]"%len(data))
+                                    " expected 6 [%d]" % len(data))
                     continue
 
                 self.is_calibrated = True if (data[0] & 0b00000100) else False
@@ -98,18 +104,18 @@ class AHT10:
                 return False
         except Exception as e:
             logging.exception("aht10: exception encountered" +
-                              " reading data: %s"%str(e))
+                              " reading data: %s" % str(e))
             return False
 
         temp = ((data[3] & 0x0F) << 16) | (data[4] << 8) | data[5]
-        self.temp = ((temp*200) / 1048576) - 50
+        self.temp = ((temp * 200.0) / 1048576.0) - 50.0
         hum = ((data[1] << 16) | (data[2] << 8) | data[3]) >> 4
-        self.humidity = int(hum * 100 / 1048576)
+        self.humidity = int(hum * 100.0 / 1048576.0)
 
         # Clamp humidity
-        if (self.humidity > 100):
+        if self.humidity > 100:
             self.humidity = 100
-        elif (self.humidity < 0):
+        elif self.humidity < 0:
             self.humidity = 0
 
         return True
@@ -120,8 +126,8 @@ class AHT10:
 
         # Reset device
         self.i2c.i2c_write(AHT10_COMMANDS['RESET'])
-        # Wait 100ms after reset
-        self.reactor.pause(self.reactor.monotonic() + .10)
+        # Wait 20ms after reset
+        self.reactor.pause(self.reactor.monotonic() + .020)
 
     def _init_aht10(self):
         # Init device
@@ -132,7 +138,7 @@ class AHT10:
 
         if self._make_measurement():
             logging.info("aht10: successfully initialized, initial temp: " +
-                         "%.3f, humidity: %.3f"%(self.temp, self.humidity))
+                         "%.3f, humidity: %.3f" % (self.temp, self.humidity))
 
     def _sample_aht10(self, eventtime):
         if not self._make_measurement():
@@ -145,14 +151,15 @@ class AHT10:
                 % (self.temp, self.min_temp, self.max_temp))
 
         measured_time = self.reactor.monotonic()
-        print_time = self.i2c.get_mcu().estimated_print_time(measured_time)
-        self._callback(print_time, self.temp)
+        if self._callback:
+            print_time = self.i2c.get_mcu().estimated_print_time(measured_time)
+            self._callback(print_time, self.temp)
         return measured_time + self.report_time
 
     def get_status(self, eventtime):
         return {
             'temperature': round(self.temp, 2),
-            'humidity': self.humidity,
+            'humidity': round(self.humidity, 1),
         }
 
 
@@ -160,3 +167,5 @@ def load_config(config):
     # Register sensor
     pheater = config.get_printer().lookup_object("heaters")
     pheater.add_sensor_factory("AHT10", AHT10)
+    pheater.add_sensor_factory("AHT2X", AHT10)
+    pheater.add_sensor_factory("AHT20_F", AHT10)  # Alias for QIDI compatibility
